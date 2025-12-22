1dff938b561078ba17eb4b447537d9d77c515db3
Julian Garcia-Hernandez <julian.garcia.hernandez11@gmail.com>
2025-12-22 15:28:43 -0600

    prettier change

--- lib/main.dart
+++ lib/main.dart
@@ -14,38 +14,34 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: ScaleGenerator(),
-      ),
-    ),
-  );
+      home: Scaffold(body: Center(child: ScaleGenerator())),
+    );
   }
 }
 
 Map<String, List<int>> intervalSequencing = {
-  "Ionian" : [2, 2, 1, 2, 2, 2, 1],
-  "Dorian" :  [2, 1, 2, 2, 2, 1, 2],
-  "Phrygian" :  [1, 2, 2, 2, 1, 2, 2],
-  "Lydian" :  [2, 2, 2, 1, 2, 2, 1],
-  "Mixolydian" :  [2, 2, 1, 2, 2, 1, 2],
-  "Aeolian" :  [2, 1, 2, 2, 1, 2, 2],
-  "Locrian" :  [1, 2, 2, 1, 2, 2, 2],
+  "Ionian": [2, 2, 1, 2, 2, 2, 1],
+  "Dorian": [2, 1, 2, 2, 2, 1, 2],
+  "Phrygian": [1, 2, 2, 2, 1, 2, 2],
+  "Lydian": [2, 2, 2, 1, 2, 2, 1],
+  "Mixolydian": [2, 2, 1, 2, 2, 1, 2],
+  "Aeolian": [2, 1, 2, 2, 1, 2, 2],
+  "Locrian": [1, 2, 2, 1, 2, 2, 2],
 };
 
 Map<String, int> midiTonic = {
-  "C" : 60,
-  "C#" : 61,
-  "D" : 62,
-  "D#" : 63,
-  "E" : 64,
-  "F" : 65,
-  "F#" : 66,
-  "G" : 67,
-  "G#" : 68,
-  "A" : 69,
-  "A#" : 70,
-  "B" : 71,
+  "C": 60,
+  "C#": 61,
+  "D": 62,
+  "D#": 63,
+  "E": 64,
+  "F": 65,
+  "F#": 66,
+  "G": 67,
+  "G#": 68,
+  "A": 69,
+  "A#": 70,
+  "B": 71,
 };
 
 class ScaleGenerator extends StatefulWidget {
@@ -54,14 +50,15 @@ class ScaleGenerator extends StatefulWidget {
   @override
   ScaleGeneratorState createState() => ScaleGeneratorState();
 }
+
 class ScaleGeneratorState extends State<ScaleGenerator> {
   List<int>? scale = [60, 62, 64, 65, 67, 69, 71], intervalSequence = [];
   String? selectedMode = "Ionian", selectedTonic = "C";
 
-  void generateScale(){
+  void generateScale() {
     int intervalSum = 0;
     intervalSequence = intervalSequencing[selectedMode];
-    scale = intervalSequence?.map((interval){
+    scale = intervalSequence?.map((interval) {
       intervalSum += interval;
       return midiTonic[selectedTonic]! + intervalSum;
     }).toList();
@@ -72,9 +69,9 @@ class ScaleGeneratorState extends State<ScaleGenerator> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-          DropdownMenu(label: Text(
-            "Mode"
-          ), dropdownMenuEntries: <DropdownMenuEntry<String>>[
+        DropdownMenu(
+          label: Text("Mode"),
+          dropdownMenuEntries: <DropdownMenuEntry<String>>[
             DropdownMenuEntry(value: "Ionian", label: "Ionian (Major)"),
             DropdownMenuEntry(value: "Dorian", label: "Dorian"),
             DropdownMenuEntry(value: "Phrygian", label: "Phyrgian"),
@@ -83,13 +80,15 @@ class ScaleGeneratorState extends State<ScaleGenerator> {
             DropdownMenuEntry(value: "Aeolian", label: "Aeolian (Minor)"),
             DropdownMenuEntry(value: "Locrian", label: "Locrian"),
           ],
-          onSelected: (String? mode){
-              setState((){
-                selectedMode = mode;
-                generateScale();
-              });
-          },),
-          DropdownMenu(label: Text("Tonic"), 
+          onSelected: (String? mode) {
+            setState(() {
+              selectedMode = mode;
+              generateScale();
+            });
+          },
+        ),
+        DropdownMenu(
+          label: Text("Tonic"),
           dropdownMenuEntries: <DropdownMenuEntry<String>>[
             DropdownMenuEntry(value: "C", label: "C"),
             DropdownMenuEntry(value: "C#", label: "C#"),
@@ -105,17 +104,14 @@ class ScaleGeneratorState extends State<ScaleGenerator> {
             DropdownMenuEntry(value: "B", label: "B"),
           ],
           onSelected: (String? tonic) {
-            setState((){
-                selectedTonic = tonic;
-                generateScale();
+            setState(() {
+              selectedTonic = tonic;
+              generateScale();
             });
           },
-          ),
-        Text("this is the scale in its midi numbered format: $scale"),
+        ),
+        Text("this is the scale in its midi numbered format $scale"),
       ],
     );
   }
 }
-
-
-          
