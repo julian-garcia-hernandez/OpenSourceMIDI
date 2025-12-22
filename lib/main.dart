ae0ed8e3cd0162bf263128f47f46721f27f2a8e2
Julian Garcia-Hernandez <julian.garcia.hernandez11@gmail.com>
2025-12-22 15:13:05 -0600

    the scale generator now correctly supports all diatonic scales, can i get a hell yea?!

--- lib/main.dart
+++ lib/main.dart
@@ -16,18 +16,79 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Row(
-            children: [DropdownMenu(label: Text(
+          child: ScaleGenerator(),
+      ),
+    ),
+  );
+  }
+}
+
+Map<String, List<int>> intervalSequencing = {
+  "Ionian" : [2, 2, 1, 2, 2, 2, 1],
+  "Dorian" :  [2, 1, 2, 2, 2, 1, 2],
+  "Phrygian" :  [1, 2, 2, 2, 1, 2, 2],
+  "Lydian" :  [2, 2, 2, 1, 2, 2, 1],
+  "Mixolydian" :  [2, 2, 1, 2, 2, 1, 2],
+  "Aeolian" :  [2, 1, 2, 2, 1, 2, 2],
+  "Locrian" :  [1, 2, 2, 1, 2, 2, 2],
+};
+
+Map<String, int> midiTonic = {
+  "C" : 60,
+  "C#" : 61,
+  "D" : 62,
+  "D#" : 63,
+  "E" : 64,
+  "F" : 65,
+  "F#" : 66,
+  "G" : 67,
+  "G#" : 68,
+  "A" : 69,
+  "A#" : 70,
+  "B" : 71,
+};
+
+class ScaleGenerator extends StatefulWidget {
+  const ScaleGenerator({super.key});
+
+  @override
+  ScaleGeneratorState createState() => ScaleGeneratorState();
+}
+class ScaleGeneratorState extends State<ScaleGenerator> {
+  List<int>? scale = [60, 62, 64, 65, 67, 69, 71], intervalSequence = [];
+  String? selectedMode = "Ionian", selectedTonic = "C";
+
+  void generateScale(){
+    int intervalSum = 0;
+    intervalSequence = intervalSequencing[selectedMode];
+    scale = intervalSequence?.map((interval){
+      intervalSum += interval;
+      return midiTonic[selectedTonic]! + intervalSum;
+    }).toList();
+    scale?.insert(0, midiTonic[selectedTonic]!);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+          DropdownMenu(label: Text(
             "Mode"
           ), dropdownMenuEntries: <DropdownMenuEntry<String>>[
-            DropdownMenuEntry(value: "Major", label: "Ionian (Major)"),
+            DropdownMenuEntry(value: "Ionian", label: "Ionian (Major)"),
             DropdownMenuEntry(value: "Dorian", label: "Dorian"),
             DropdownMenuEntry(value: "Phrygian", label: "Phyrgian"),
             DropdownMenuEntry(value: "Lydian", label: "Lydian"),
-            DropdownMenuEntry(value: "Mixolydian", label: "Mixoldyian"),
-            DropdownMenuEntry(value: "Minor", label: "Aeolian (Minor)"),
+            DropdownMenuEntry(value: "Mixolydian", label: "Mixolydian"),
+            DropdownMenuEntry(value: "Aeolian", label: "Aeolian (Minor)"),
             DropdownMenuEntry(value: "Locrian", label: "Locrian"),
-          ]),
+          ],
+          onSelected: (String? mode){
+              setState((){
+                selectedMode = mode;
+                generateScale();
+              });
+          },),
           DropdownMenu(label: Text("Tonic"), 
           dropdownMenuEntries: <DropdownMenuEntry<String>>[
             DropdownMenuEntry(value: "C", label: "C"),
@@ -42,10 +103,19 @@ class MainApp extends StatelessWidget {
             DropdownMenuEntry(value: "A", label: "A"),
             DropdownMenuEntry(value: "A#", label: "A#"),
             DropdownMenuEntry(value: "B", label: "B"),
-          ])
-          ]),
-        ),
-      ),
+          ],
+          onSelected: (String? tonic) {
+            setState((){
+                selectedTonic = tonic;
+                generateScale();
+            });
+          },
+          ),
+        Text("this is the scale in its midi numbered format: $scale"),
+      ],
     );
   }
 }
+
+
+          
