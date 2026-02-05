4041e21a41e0290c08ab1fb46159c568c0ab335d
Julian Garcia-Hernandez <julian.garcia.hernandez11@gmail.com>
2026-02-05 13:02:37 -0600

    created the learn screen (aka home screen) widget

---
+++ lib/screens/learn.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class LearnScreen extends StatelessWidget {
+  const LearnScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Learn Screen")),
+      body: Center(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.music_note), label: "Learn"),
+          // BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"), TODO: gonna implement later
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text("Settings"),
+              onTap: () {
+                // setState(() {}); //TODO: gonna implement later
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
