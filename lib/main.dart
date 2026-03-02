48e4a2dcee5e337849653653b4d11f18457d7926
Julian Garcia-Hernandez <julian.garcia.hernandez11@gmail.com>
2026-03-02 14:07:00 -0600

    renamed some files more appropriately

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import './screens/learn.dart';
+import 'screens/learn_screen.dart';
 import 'package:flutter/material.dart';
 // import 'package:flutter_midi_command/flutter_midi_command.dart'; TODO: implement MIDI functionality
 
